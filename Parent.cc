4b718a4d2df77bc3497a57b8f9b163c94af1c3a7
Qingnan Duan <duanqn_own_1@yeah.net>
2017-11-27 23:24:32 -0500

    Implement Parent class (_Task)

---
+++ Parent.cc
@@ -0,0 +1,31 @@
+#include "Parent.h"
+#include "MPRNG.h"
+
+extern MPRNG g_random;
+
+Parent::Parent(Printer &printer, Bank &bank, unsigned int numStudents, unsigned int parentalDelay):
+m_printer(&printer),
+m_bank(&bank),
+m_numStudents(numStudents),
+m_delay(parentalDelay){}
+
+void Parent::main(){  // private
+  m_printer->print(Printer::Kind::Parent, 'S'); // print: starting
+  while(true){
+    _Accept(~Parent){
+      break;  // exit on destructor call
+    }
+    _Else{
+      for(int i = 0; i < m_delay; ++i){
+        yield();
+      }
+      // generate the amount and target for a gift
+      unsigned int money = g_random(1, 3);
+      unsigned int student = g_random(0, m_numStudents - 1);
+      // deposit the money into the bank
+      m_printer->print(Printer::Kind::Parent, 'D', student, money); // print: giving money
+      m_bank->deposit(student, money);
+    }
+  }
+  m_printer->print(Printer::Kind::Parent, 'F'); // print: finished
+}
