20bf0d5702c4887476826ac5224b4b69740ebe83
Qingnan Duan <dqn14@mails.tsinghua.edu.cn>
2017-12-01 20:04:06 -0500

    Modify test for Printer

--- test/driverPrinter.cc
+++ test/driverPrinter.cc
@@ -9,7 +9,9 @@ int main(){
     printer.print(Printer::Kind::Groupoff, 'S');
     printer.print(Printer::Kind::Parent, 'D', 1, 3);
     printer.print(Printer::Kind::Student, 0, 'S');
-    printer.print(Printer::Kind::Vending, 2, 'S', 2);
+    printer.print(Printer::Kind::Vending, 1, 'S', 2);
+    printer.print(Printer::Kind::Vending, 1, 'r');
+    printer.print(Printer::Kind::Vending, 1, 'R');
   }
   std::cerr << "Test ends" << std::endl;
   return 0;
