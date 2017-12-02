d556dc5d974865f6e075ab01ecc4b32ef633a31f
Qingnan Duan <dqn14@mails.tsinghua.edu.cn>
2017-12-02 00:38:08 -0500

    Change header guard for WATCardOffice.h

--- WATCard.h
+++ WATCard.h
@@ -1,5 +1,4 @@
-#ifndef WATCARD_H_
-#define WATCARD_H_
+#pragma once
 
 #include <uFuture.h>
 
@@ -16,6 +15,4 @@ class WATCard{
   void withdraw(unsigned int amount); // take some money out from WATCard
   unsigned int getBalance();  // get the current amount of money inside WATCard
 };
-
-#endif  // WATCARD_H_
 
